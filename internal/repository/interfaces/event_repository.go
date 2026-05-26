5254974b36d61485deb92ff1486d6c8d8008f3b1
jalvess021 <jalvess021@gmail.com>
2026-05-26 12:08:34 -0300

    feat: ports de repository como contratos de infraestrutura.

---
+++ internal/repository/interfaces/event_repository.go
@@ -0,0 +1,8 @@
+package interfaces
+
+import "github.com/jalvess021/capital-pipefy/internal/domain"
+
+type EventRepository interface {
+    Save(event *domain.ProcessedEvent) error
+    ExistsByEventID(eventID string) (bool, error)
+}
