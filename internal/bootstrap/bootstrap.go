fe1194376783a52dad2a4748e25a48962ec421a7
jalvess021 <jalvess021@gmail.com>
2026-05-26 13:39:28 -0300

    feat: bootstrap com App struct e injecao de repositorios

--- internal/bootstrap/bootstrap.go
+++ internal/bootstrap/bootstrap.go
@@ -1,20 +1,34 @@
 package bootstrap
 
 import (
-	"github.com/jalvess021/capital-pipefy/internal/config"
-	"github.com/jalvess021/capital-pipefy/internal/database"
+    "github.com/jalvess021/capital-pipefy/internal/config"
+    "github.com/jalvess021/capital-pipefy/internal/database"
+    "github.com/jalvess021/capital-pipefy/internal/repository"
+    postgresrepo "github.com/jalvess021/capital-pipefy/internal/repository/postgres"
 )
 
-func NewApp() (*config.Config, database.Database, error) {
-	cfg, err := config.Load()
-	if err != nil {
-		return nil, nil, err
-	}
+type App struct {
+    Config     *config.Config
+    DB         *database.PostgresDB
+    ClientRepo repository.ClientRepository
+    EventRepo  repository.EventRepository
+}
 
-	db, err := database.NewPostgres(cfg.DatabaseURL)
-	if err != nil {
-		return nil, nil, err
-	}
+func NewApp() (*App, error) {
+    cfg, err := config.Load()
+    if err != nil {
+        return nil, err
+    }
 
-	return cfg, db, nil
+    db, err := database.NewPostgres(cfg.DatabaseURL)
+    if err != nil {
+        return nil, err
+    }
+
+    return &App{
+        Config:     cfg,
+        DB:         db,
+        ClientRepo: postgresrepo.NewClientRepository(db.GormDB()),
+        EventRepo:  postgresrepo.NewEventRepository(db.GormDB()),
+    }, nil
 }
